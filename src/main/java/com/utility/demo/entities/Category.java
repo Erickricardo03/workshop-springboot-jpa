c1ca9b5dc3fd7657ea3c98030fa749afcd0c4ade
Erick Ricardo <edck030303@gmail.com>
2024-09-22 17:41:43 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/utility/demo/entities/Category.java
+++ src/main/java/com/utility/demo/entities/Category.java
@@ -5,12 +5,15 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
+
 
 
 @Entity
@@ -25,47 +28,74 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
-	@Transient
-	private Set<Product> products = new HashSet<>();
 	
-	public Long getId() {
-		return id;
+	   public Category() {
+	   }
+	   
+	    
+    @JsonIgnore
+	@ManyToMany(mappedBy = "categories") 
+	private Set<Product> products = new HashSet<>();
+
+	
+	    public Category(Long id, String name, Set<Product> products) {
+	        this.id = id;
+	        this.name = name;
+	        this.products = products;
+	    }
+
+	    public Category(Object object, String string) {
+		
+		}
+
+		public Long getId() {
+	        return id;
+	    }
+
+	    public void setId(Long id) {
+	        this.id = id;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public Set<Product> getProducts() {
+	        return products;
+	    }
+
+	    public void setProducts(Set<Product> products) {
+	        this.products = products;
+	    }
+
+	    @Override
+	    public int hashCode() {
+	        return Objects.hash(id);
+	    }
+
+	    @Override
+	    public boolean equals(Object obj) {
+	        if (this == obj) return true;
+	        if (obj == null || getClass() != obj.getClass()) return false;
+	        Category other = (Category) obj;
+	        return Objects.equals(id, other.id);
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Category{" +
+	                "id=" + id +
+	                ", name='" + name + '\'' +
+	                ", products=" + products +
+	                '}';
+	    
+
+	   }
 	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Category other = (Category) obj;
-		return Objects.equals(id, other.id);
-	}
-	public Category(Long id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-	}
-	public Set<Product> getProducts() {
-		return products;
-	}
+	
 	
 
-}
--- src/main/java/com/utility/demo/entities/Product.java
+++ src/main/java/com/utility/demo/entities/Product.java
@@ -9,36 +9,31 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
-
 
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
-    private static final long serialVersionUID = 1L;
-    
-    
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY )
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String descreption;
 	private Double price;
 	private String imgUrl;
-	
-	
-	@Transient
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns =  @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
+	public Product() {
 
-	public Product( ) {
-		
-		
-		
 	}
-
 
 	public Product(Long id, String name, String descreption, Double price, String imgUrl) {
 		super();
@@ -49,67 +44,62 @@ public class Product implements Serializable {
 		this.imgUrl = imgUrl;
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getDescreption() {
 		return descreption;
 	}
-
 
 	public void setDescreption(String descreption) {
 		this.descreption = descreption;
 	}
 
-
 	public Double getPrice() {
 		return price;
 	}
-
 
 	public void setPrice(Double price) {
 		this.price = price;
 	}
 
-
 	public String getImgUrl() {
 		return imgUrl;
 	}
-
 
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
 
-
 	public Set<Category> getCategories() {
 		return categories;
 	}
 
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -123,5 +113,18 @@ public class Product implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
+	public Product(Long id, String name, String descreption, Double price, String imgUrl, Set<Category> categories) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.descreption = descreption;
+		this.price = price;
+		this.imgUrl = imgUrl;
+		this.categories = categories;
+	}
 
-}
+	
+	}
+
+	
+
