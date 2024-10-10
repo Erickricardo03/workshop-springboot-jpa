cb81e3ee42cbd224b088e567dd2c5454945ed360
Erick Ricardo <edck030303@gmail.com>
2024-10-09 23:11:32 -0300

    User update

--- src/main/java/com/utility/demo/services/UserService.java
+++ src/main/java/com/utility/demo/services/UserService.java
@@ -38,6 +38,20 @@ public void delete(Long id) {
 	
 }
 
+public User update(Long id, User obj) {
+	User entity = repository.getReferenceById(id);
+	updateData(entity, obj);
+	return repository.save(entity);
+}
+
+
+private void updateData(User entity, User obj) {
+	entity.setName(obj.getName());
+	entity.setEmail(obj.getEmail());
+	entity.setFone(obj.getFone());
+	
+}
+
 
 }
 
